e0dd80c0e48663f7d8ec02cd815140ad18a04792
therafiniac <therafiniac@gmail.com>
2025-03-24 18:09:12 +0530

    add logout functionality and working on middleware

---
+++ src/middleware.ts
@@ -0,0 +1,13 @@
+import { NextResponse } from "next/server";
+import { NextRequest } from "next/server";
+
+export function middleware(request: NextRequest) {
+  const path = request.nextUrl.pathname;
+  const isPublicPath = path === "/login" || path === "/signup";
+  const token = request.cookies.get("token")?.value || "";
+
+}
+
+export const config = {
+  matcher: ["/", "/profile", "/login", "/signup"],
+};
